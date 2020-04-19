22418122290dd52170cbcfd2ab51e67422c87963
AtomJump <peter@atomjump.com>
2020-04-20 11:34:25 +1200

    deleting

--- index.php
+++ index.php
@@ -263,8 +263,8 @@
 			
 			global $cnf;
 			
-			echo "Checking if we are to delete layer: " . $cnf['db']['deleteDeletes'];
-			if(($cnf['db']['deleteDeletes']) && ($cnf['db']['deleteDeletes'] === true)) {
+			echo "Checking if we are to delete layer " . $this_layer . ": " . $cnf['db']['deleteDeletes'] . "\n";
+			if(($cnf['db']['deleteDeletes']) && ($cnf['db']['deleteDeletes'] == true)) {
 				//Now delete the layer itself
 				if($preview == false) {
 					error_log("Deleting layer " . $this_layer);
